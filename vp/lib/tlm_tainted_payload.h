d2b5d9fa5b22226c64d901abaee6e72ac658734e
Pascal Pieper <ppieper@fb3.uni-bremen.de>
2018-12-13 14:42:56 +0100

    tlm exension?

---
+++ vp/lib/tlm_tainted_payload.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <systemc>
+#include <tlm_generic_payload/tlm_gp.h>
+
+class tlm_tainted_payload;
+
+class tlm_tainted_payload : public tlm::tlm_extension<tlm_tainted_payload>
+{
+	//TODO: Do shits
+};
