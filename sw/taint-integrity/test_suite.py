2d90a7edc627373a8bdfe42d2bb236a6a6c9d8e5
Pascal Pieper <accounts@pascalpieper.de>
2020-03-24 16:30:14 +0100

    IIncorporated new vp name

--- sw/taint-integrity/test_suite.py
+++ sw/taint-integrity/test_suite.py
@@ -34,7 +34,7 @@ attacks = [(-4, 'Direct', 'Stack', 'Function Pointer (param)'),
 for counter, attack in enumerate(attacks, 1):
     print(str(counter) + ": " + attack[1] + " " + attack[2] + " on " + attack[3], end="\n\t")
     try:
-        output = subprocess.check_output("riscv-vp atts --parameter " + str(attack[0]) + " 2> /dev/null", shell=True)
+        output = subprocess.check_output("riscv-dfa atts --parameter " + str(attack[0]) + " 2>&1", shell=True)
         if "ATTACK successful" in str(output):
             print ("Oh no, attack " + str(attack[0]) + " was successful")
             exit (-1)
